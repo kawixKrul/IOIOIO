c19f2702b22325db731a6b8427ccbe073d644b6d
wiktorperczak <wiktor.prczk@gmail.com>
2025-04-15 22:57:38 +0200

    README.md

--- app/server/IO/src/main/kotlin/com/Application.kt
+++ app/server/IO/src/main/kotlin/com/Application.kt
@@ -1,4 +1,4 @@
-package com.example
+package com
 
 import io.ktor.server.application.*
 
---
+++ app/server/IO/src/main/kotlin/com/Security.kt
@@ -0,0 +1,6 @@
+package com
+
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+}
