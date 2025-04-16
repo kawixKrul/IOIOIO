639c83daa560cf1cbcedf0c2a07e33f314aa304f
wiktorperczak <wiktor.prczk@gmail.com>
2025-04-16 02:45:59 +0200

    Student service usage example

--- app/server/IO/src/main/kotlin/com/Application.kt
+++ app/server/IO/src/main/kotlin/com/Application.kt
@@ -5,9 +5,6 @@ import com.database.connectToDatabase
 import com.database.createTables
 import com.database.table.Students
 import io.ktor.server.application.*
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -23,18 +20,24 @@ fun Application.module() {
     println("Database connected and all tables are created.")
 
     val studentService = StudentService()
-    
+
+    // Add new student
 //    studentService.addStudent(
-//        mail = "test1@example.com",
-//        password = "secret",
-//        name = "Maria",
-//        surname = "Kowalska"
+//        mail = "bialecki@example.com",
+//        password = "marcinbialecki1",
+//        name = "Marcin",
+//        surname = "Białecki"
 //    )
 //    println("New student added.")
 
+    // Delete student
+    studentService.deleteStudent(3)
+    println("Student deleted.")
 
+    // All students printed
     println("All students:")
     studentService.getAllStudents().forEach { student ->
-        println("ID: ${student[Students.id]}, Email: ${student[Students.mail]}, Name: ${student[Students.name]} ${student[Students.surname]}")
+        println("ID: ${student[Students.id]}, Email: ${student[Students.mail]}, Password: ${student[Students.password]}," +
+                "Name: ${student[Students.name]}, Surname: ${student[Students.surname]}")
     }
 }
