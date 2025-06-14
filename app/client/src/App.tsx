13564fe3b8a9bea23c98f2a0ca0b3683846e7632
mlodyjesienin <filipzielinski377@gmail.com>
2025-06-14 16:20:16 +0200

    refactor: rename overseer to supervisor

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,10 +1,11 @@
 import { LandingPage } from './components/landing-page'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import UserPage from './pages/user-page'
-import OverseerPage from './pages/overseer-page'
+import OverseerPage from './pages/supervisor-page'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 import { ProtectedRoute } from './components/ProtectedRoute'
+import SupervisorPage from './pages/supervisor-page'
 
 // Create a query client instance
 const queryClient = new QueryClient({
@@ -19,6 +20,13 @@ const queryClient = new QueryClient({
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/supervisor" element={<SupervisorPage />} />
+        </Routes>
+      </BrowserRouter>
       <AuthProvider>
         <BrowserRouter>
           <Routes>
