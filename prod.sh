682f10cae63cac9a49bcc0251dcf724e1f76e88f
kawixKrul <macioukos@gmail.com>
2025-06-20 20:38:10 +0200

    prod deploy1

---
+++ prod.sh
@@ -0,0 +1,5 @@
+docker tag ioioio-backend kawixking/ioioio-backend:latest
+docker tag ioioio-frontend kawixking/ioioio-frontend:latest
+
+docker push kawixking/ioioio-backend:latest
+docker push kawixking/ioioio-frontend:latest
